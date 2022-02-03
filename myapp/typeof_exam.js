3c77cf2441adbfc4be37ef74151ec33b9be9ae0b
ggh93 <ggh93@fandomberry.com>
2022-02-03 18:57:40 +0900

    타입체크 수정

--- myapp/typeof_exam.js
+++ myapp/typeof_exam.js
@@ -1,12 +1,56 @@
 function sum(a, b) {
   // a와 b가 number 타입인지 체크
-  if (typeof a !== 'number') {
-    return console.log('a가 number타입이 아닙니다.');
-  } else if (typeof b !== 'number') {
-    return console.log('b가 number타입이 아닙니다.');
-  } else {
-    return console.log(a + b);
+  const getTypeNameToA = getType(a);
+  const getTypeNameToB = getType(b);
+  if (!isNumber(a) || !isNumber(b)) {
+    throw new TypeError(
+      `A 파라미터에 ${getTypeNameToA} 타입의 값이, B 파라미터에 ${getTypeNameToB} 타입의 값이 할당되었습니다.`
+    );
   }
+  return a + b;
+}
+console.log(sum(10, 20)); // 30
+console.log(sum(new Date(), function () {})); // TypeError
+
+function getType(target) {
+  return Object.prototype.toString.call(target).slice(8, -1);
+}
+function isString(target) {
+  return getType(target) === 'String';
 }
 
-sum(1, 2); // 'xy'
+function isNumber(target) {
+  return getType(target) === 'Number';
+}
+
+function isBoolean(target) {
+  return getType(target) === 'Boolean';
+}
+
+function isNull(target) {
+  return getType(target) === 'Null';
+}
+
+function isUndefined(target) {
+  return getType(target) === 'Undefined';
+}
+
+function isObject(target) {
+  return getType(target) === 'Object';
+}
+
+function isArray(target) {
+  return getType(target) === 'Array';
+}
+
+function isDate(target) {
+  return getType(target) === 'Date';
+}
+
+function isRegExp(target) {
+  return getType(target) === 'RegExp';
+}
+
+function isFunction(target) {
+  return getType(target) === 'Function';
+}
